reformulate GN dx backprop. still wrong

playground/cs231n/assignment2/cs231n/layers.py
@@ -789,18 +789,17 @@ def spatial_groupnorm_forward(x, gamma, beta, G, gn_param):
     ###########################################################################
     N, C, H, W = x.shape
     x = x.reshape(N, G, C // G, H, W)
-    sample_mean = np.mean(x, axis=(2,3,4))
-    sample_var = np.var(x, axis=(2,3,4))
+    sample_mean = np.mean(x, axis=(2,3,4), keepdims=True)
+    sample_var = np.var(x, axis=(2,3,4), keepdims=True)
 
     vareps = sample_var + eps
-    x_normalized = (x.T - sample_mean) / np.sqrt(vareps)
+    x_normalized = (x - sample_mean) / np.sqrt(vareps)
     x = x_normalized.reshape(N, C, H, W)
     out = x * gamma + beta
 
     cache = (x, gamma, sample_mean, vareps, x_normalized, G)
 
     return out, cache
-
 
 def spatial_groupnorm_backward(dout, cache):
     """
@@ -828,13 +827,10 @@ def spatial_groupnorm_backward(dout, cache):
     vareps = cache[3]
     x_normalized = cache[4]
     G = cache[5]
-    D = C*H*W
-
+    D = (C//G)*H*W
+    print('D', D)
     x = x.reshape(N, G, C // G, H, W)
 
-    x_mu = x.T - sample_mean
-    dx_norm = dout * gamma
-    dx_norm = dx_norm.reshape(N, G, C // G, H, W)
 
     dout_r = dout.reshape(N, G, C // G, H, W)
     dbeta = np.sum(dout_r, axis=(0,3,4))
@@ -845,14 +841,21 @@ def spatial_groupnorm_backward(dout, cache):
     dgamma = dgamma.reshape(1, C, 1, 1)
 
 
+    x_mu = x - sample_mean
+    dx_norm = dout * gamma
+    dx_norm = dx_norm.reshape(N, G, C // G, H, W)
+
     std_inv = 1/np.sqrt(vareps)
 
-    dvar = np.sum(dx_norm * x_mu.T, axis=(2,3,4)) * -.5 * np.power(vareps, -3/2)
+    dvar = np.sum(dx_norm * x_mu, axis=(2,3,4)) *\
+        -.5 * np.power(vareps, -3/2).T
 
-    dmu = np.sum(dx_norm.T * -std_inv,
-                 axis=(0,1,2)) + dvar * np.mean(-2. * x_mu.T, axis=(2,3,4))
+    dmu_term1 = np.sum((dx_norm * -std_inv), axis=(2,3,4))
 
-    dx = (dx_norm.T * std_inv) + (dvar * 2 * x_mu / D) + (dmu / D)
+    dmu = dmu_term1 + \
+        dvar * np.mean(-2. * x_mu, axis=(2,3,4))
+
+    dx = (dx_norm * std_inv) + (dvar.T * 2 * x_mu / D) + (dmu.T / D)
 
     dx = dx.reshape(N, C, H, W)
 