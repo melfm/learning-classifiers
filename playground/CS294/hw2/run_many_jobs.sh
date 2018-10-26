8891c8983b60da9572046f96398fc7debd9198c0
Melissa Mozifian <melissafm24@gmail.com>
2018-10-26 12:21:56 -0400

    update experiment scripts

--- playground/CS294/hw2/run_many_jobs.sh
+++ playground/CS294/hw2/run_many_jobs.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
-batches=( 10000 30000 50000)
+batches=(10000 30000 50000)
 learning_rates=(0.005 0.01 0.02)
 for i in "${batches[@]}"
 do
     for j in "${learning_rates[@]}"
-do
-    echo 'Running with batch'
-    echo i
-    echo 'And learning rate '
-    echo j
-    sh train_policy_job.sh $i $j
+    do
+		echo"Running with batch and learning rate"
+        echo "$i"
+		echo "$j"
+		sh train_policy_job.sh $i $j
+    done
 done
-
--- playground/CS294/hw2/train_policy_job.sh
+++ playground/CS294/hw2/train_policy_job.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-python train_pg_f18.py HalfCheetah-v2 -ep 1500 --discount 0.9 -n 100 -e 3 -l 2 -s 32 -b $1 -lr $2 -rtg --exp_name hc_b10000_r0.005
+python train_pg_f18.py HalfCheetah-v2 -ep 1500 --discount 0.9 -n 100 -e 3 -l 2 -s 32 -b $1 -lr $2 -rtg --exp_name hc_b$1_r$2
 echo 'Done.'
 
 
